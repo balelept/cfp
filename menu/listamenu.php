e30504ed48b802d6c57664b86c2f2c6df1d5f574
balelept <balelegaming@gmail.com>
2017-10-17 01:55:24 +0100

    Lista do menu

---
+++ menu/listamenu.php
@@ -0,0 +1,69 @@
+<ul>
+   <li class='active has-sub'><a href='#'><span>Ação de formação</span></a>
+      <ul>
+             <li class='first-child'><a href="acao.php"><span>Lista de ações</span></a> </li>
+              <li class='child'><a href="acao_insert.php?i=1"><span>Inserir ação</span></a> </li>
+             <li class='child'><a href="acao.php"><span>Atribuir notas</span></a>         </li>
+             <li class='child'><a href="acao.php"><span>Alterar notas</span></a>         </li>
+             <li class='child'><a href="acao.php"><span>Criar turma</span></a>         </li>
+
+
+
+
+
+         <li class='has-sub'><a href='#'><span>Atualizar base de dados</span></a>
+             <ul>
+             <li class='first-child'><a href="grupo_recrutamento.php"><span>Grupos de recrutamento</span></a> </li>
+              <li class='child'><a href="modalidade.php"><span>Modalidades</span></a>         </li>
+              <li class='last-child'><a href="area_formacao.php"><span>Área de formação</span></a>         </li>
+
+         </ul>
+        </li>
+       </ul>
+      </li>
+
+         <li class='active has-sub'><a href='#'><span>Formandos</span></a>
+      <ul>
+             <li class='first-child'><a href="formando.php"><span>Lista de formandos</span></a> </li>
+
+             <li class='child'><a href="turma.php"><span>Criar turma</span></a>         </li>
+             <li class='last-child'><a href="formando_insert.php?i=1"><span>Inserir formando</span></a> </li>
+
+
+
+         <li class='has-sub'><a href='#'><span>Atualizar base de dados</span></a>
+             <ul>
+             <li class='first-child'><a href="habilitacao_academica.php"><span>Hablitação académica</span></a> </li>
+              <li class='child'><a href="escalao.php"><span>Escalão</span></a>         </li>
+              <li class='child'><a href="agrupamento.php"><span>Agrupamentos</span></a>         </li>
+              <li class='child'><a href="cfp.php"><span>Centros de formação</span></a>         </li>
+              <li class='last-child'><a href="escola.php"><span>Escolas</span></a>         </li>
+
+         </ul>
+      </li>
+   </ul>
+</li>
+
+           <li class='active has-sub'><a href='#'><span>Formadores</span></a>
+      <ul>
+             <li class='first-child'><a href="formador.php"><span>Lista de formadores</span></a> </li>
+             <li class='last-child'><a href="formador_insert.php?i=1"><span>Inserir formador</span></a> </li>
+
+
+
+         <li class='has-sub'><a href='#'><span>Atualizar base de dados</span></a>
+             <ul>
+             <li class='first-child'><a href="habilitacao_academica.php"><span>Hablitação académica</span></a> </li>
+              <li class='child'><a href="escalao.php"><span>Escalão</span></a>         </li>
+              <li class='child'><a href="agrupamento.php"><span>Agrupamentos</span></a>         </li>
+              <li class='child'><a href="cfp.php"><span>Centros de formação</span></a>         </li>
+              <li class='last-child'><a href="escola.php"><span>Escolas</span></a>         </li>
+
+
+         </ul>
+      </li>
+   </ul>
+</li>
+
+   <li class='last'><a href='sobre.php'><span>Indormações</span></a></li>
+</ul>
